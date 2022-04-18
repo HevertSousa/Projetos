71b5387bfbc1866f4a385c3cace30137e49c04ef
HevertSousa <hevert.sousa@outlook.com>
2022-04-18 16:02:45 -0300

    modificações

--- Inicio/app.js
+++ Inicio/app.js
@@ -1,28 +1,5 @@
-var soma_func = require("./calculadora");
+var calculadora = require("./calculadora");
 
-function mult(a,b){
-    return a*b;
-}
-function sub(a,b){
-    return a-b;
-}
-function div(a,b){
-    return a/b;
-}
-console.log(soma_func(10,20));
-var resultado = mult(10,10);
-var resultado_sub = sub(20,40);
-console.log(div(10,2));
-console.log(resultado);
-console.log(resultado_sub);
-
-
-/* var mostraSite = false;
-const site = "www.guiadoprogramador.com";
-console.log("Hello World");
-console.log("Meu nome é Hevert");
-console.log("Estou aprendendo javaScript com node");
-
-if(mostraSite){ //se mostrar site 
-    console.log(site);//Printe o site
-} */
+console.log(calculadora.nome); //as variáveis também podem ser modificadas em outro módulo.
+console.log(calculadora.soma(10,20));
+console.log(calculadora.mult(30,50));
--- Inicio/calculadora.js
+++ Inicio/calculadora.js
@@ -1,5 +1,25 @@
+var nome = "minha calculadora V1";
+
 function soma(a,b){
     return a+b;
 }
 
-module.exports =  soma;
+function mult(a,b){
+    return a*b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+module.exports = { //Multiplas funções podem ser exportadas em blocos como pode ser observado. 
+    soma,
+    mult,
+    sub,
+    div,
+    nome
+}; // também podem ser exportadas variáveis

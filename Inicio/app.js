949796109124b85df6579735d32bc14a602c4938
HevertSousa <hevert.sousa@outlook.com>
2022-04-18 14:52:59 -0300

    Create app.js
    Início das atividades com NodeJS

---
+++ Inicio/app.js
@@ -0,0 +1,10 @@
+var mostraSite = false;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World");
+console.log("Meu nome é Hevert");
+console.log("Estou aprendendo javaScript com node");
+
+if(mostraSite){ //se mostrar site 
+    console.log(site);//Printe o site
+}

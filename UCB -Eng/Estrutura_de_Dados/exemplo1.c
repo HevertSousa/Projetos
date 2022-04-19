73acace2cee6b3d8dcdcbd21144d883b48f3ef3d
HevertSousa <hevert.sousa@outlook.com>
2022-04-19 18:45:30 -0300

    Calcular médias

--- UCB -Eng/Estrutura_de_Dados/exemplo1.c
+++ UCB -Eng/Estrutura_de_Dados/exemplo1.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <locale.h>
+
+float calculaMedia(float nota1, float nota2){
+     return (nota1+nota2)/2;
+    
+}
 
 int main(){
 
 
+    setlocale(LC_ALL,"");
+
+    // crie um algoritmo que receba duas notas e mostre a média entre elas
+    float nota1, nota2, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+    media = calculaMedia(nota1, nota2);
+
+    printf("A média das notas é %.2f", media);
 
     return 0;
 }

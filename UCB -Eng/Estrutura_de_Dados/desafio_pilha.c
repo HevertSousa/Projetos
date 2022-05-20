7dced58d029f4bb2e81b502e0394280149669fbe
HevertSousa <hevert.sousa@outlook.com>
2022-05-20 17:44:19 -0300

    atualizacao

--- UCB -Eng/Estrutura_de_Dados/desafio_pilha.c
+++ UCB -Eng/Estrutura_de_Dados/desafio_pilha.c
@@ -11,11 +11,11 @@ typedef struct p{
 
 }Pilha, *P_pilha;
 
-void mostrar(Pilha *elemeno){
+void mostrar(Pilha *elemento){
 
     printf("%d\n", elemento->prox);
     if(elemento->prox != NULL){
-        mostrar(elemento->pox);
+        mostrar(elemento->prox);
     }
 }
 P_pilha empilharTopo(Pilha *topo, Pilha *novo){
@@ -54,8 +54,44 @@ int main(){
     topo = &elemento1;
     base = &elemento1;
     
+    // adicionando p2 na pilha 
 
+    p1-> prox = p2;
+    p2-> ant = p1;
+    topo = p2; // o topo da pilha agora é o novo elemento
+     mostrar(base);
 
+     printf("\n------------------------\n");
+
+     Pilha elemento3; 
+     elemento3.ant = NULL;
+     elemento3.p = 30;
+     elemento3.prox = NULL;
+
+     topo = empilharTopo(topo, &elemento3);
+     mostrar(base);
+
+     printf("\n--------------------\n");
+
+     Pilha elemento4;
+     elemento4.ant = NULL;
+     elemento4.p = 40;
+     elemento4.prox = NULL;
+
+     topo = empilharTopo(topo, &elemento4);
+     mostrar(base);
+
+     printf("\n--------------\n");
+
+     Pilha elemento5;
+     elemento5.ant = NULL;
+     elemento5.p = 50;
+     elemento5.prox = NULL;
+
+     topo = empilharTopo(topo, &elemento5);
+     mostrar(base);
+
+     printf("\n-----------------\n");
 
     return 0;
 }

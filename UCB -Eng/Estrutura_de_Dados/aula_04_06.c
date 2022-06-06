fd5b71e49838cfe74f16826e72f91b46f588b214
HevertSousa <hevert.sousa@outlook.com>
2022-06-06 13:43:22 -0300

    novas aulas

--- UCB -Eng/Estrutura_de_Dados/aula_04_06.c
+++ UCB -Eng/Estrutura_de_Dados/aula_04_06.c
@@ -10,59 +10,47 @@
 #include <conio.h>
 #include <locale.h>
 
-int menu (void);
+typedef struct no{
+    char item;
+    struct no *direita;
+    struct no *esquerda;
+}NO;
 
-int main (void){
-	int opcao;
-	setlocale(LC_ALL, "portuguese");
-	do{
-		opcao = menu();
-		switch(opcao){
-			case 1:
-				printf("\nINCLUIR\n");
-				break;
-			case 2:
-				printf("\nLISTAR\n");
-				break;
-			case 3:
-				printf("\nALTERAR\n");
-				break;
-			case 4:
-				printf("\nEXCLUIR\n");
-				break;
-			case 5:
-				printf("\nDESTRUIR\n");
-				break;
-			case 6:
-				printf("\nSAIR\n");
-				break;
-		}
-	}while(opcao != 0);
-	
+typedef struct Arvore{
+    NO *raiz;
+}r;
+
+void inicia_arvore(struct Arvore *r){
+    if(r->raiz != NULL){
+       NO *novo = (NO*)malloc(sizeof(NO));
+        r->raiz = NULL;
+       printf("Árvore alocada com sucesso!!!");
+
+    }else{
+        printf("Erro de Alocação");
+    }
 }
 
-
-
-int menu (void){
-	int opcao;
-	do{		  
-			printf("\n=====================");
-			printf("\n|**MENU DE SELEÇÃO**|");
-			printf("\n=====================");
-			printf("\n=== 1 INCLUIR\t=====");
-			printf("\n=== 2 LISTAR\t=====");
-			printf("\n=== 3 ALTERAR\t=====");
-			printf("\n=== 4 EXCLUIR\t=====");
-			printf("\n=== 5 DESTRUIR\t=====");
-			printf("\n=== 0 SAIR\t=====");
-			printf("\n=====================\n");
-			fflush(stdin);
-			scanf("%d", &opcao);
-			if(opcao < 0 || opcao > 5){
-				system("cls");
-				printf("\n\n***ATENÇÃO ENTRE COM UMA OPÇÃO VÁLIDA!!!");
-			}
-	}while(opcao < 0 || opcao > 5); 
-	system("cls");
-	return opcao;
+void inserir(r *dado, int item){
+    if(dado->raiz == NULL){
+        dado->raiz =(NO*)malloc(sizeof(NO));
+        dado->raiz->direita = NULL;
+        dado->raiz->esquerda = NULL;
+        dado->raiz->item = item;
+    }else{
+        if(dado->raiz->direita->item > item){
+            dado->raiz->direita = NULL;
+            dado->raiz->item = item;
+        }else{
+            dado->raiz->esquerda = NULL;
+            dado->raiz->item = item;
+        }
+    }
+}
+int main(){
+    
+    struct Arvore *s;
+    inicia_arvore(&s);
+    inserir(s, 5);
+    return 0;
 }

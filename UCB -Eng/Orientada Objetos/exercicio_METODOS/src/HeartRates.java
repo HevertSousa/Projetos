95414639f6cbc320b1bb66c443b1f59389ecb08b
HevertSousa <hevert.sousa@outlook.com>
2022-09-13 14:29:26 -0300

    novos_exercicios

---
+++ UCB -Eng/Orientada Objetos/exercicio_METODOS/src/HeartRates.java
@@ -0,0 +1,40 @@
+public class HeartRates {
+    
+    private String nome, sobreNome;
+    private int dia, mes, ano;
+
+
+    public String getNome(){
+        return nome;
+    }
+    public String getSobreNome(){
+        return sobreNome;
+    }
+    public int getDia(){
+        return dia;
+    }
+    public int getMes(){
+        return mes;
+    }
+    public int getAno(){
+        return ano;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setSobreNome(String sobreNome){
+        this.sobreNome = sobreNome;
+    }
+    public void setDia(int dia){
+        this.dia = dia;
+    }
+    public void setMes(int mes){
+        this.mes = mes;
+    }
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+
+    
+
+}

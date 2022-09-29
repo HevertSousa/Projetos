210d18e118d91e69b913111796413031761f2622
HevertSousa <hevert.sousa@outlook.com>
2022-09-29 10:57:54 -0300

    treinando_java

---
+++ UCB -Eng/Orientada Objetos/extendsAula/src/extendsaula/Aplicacao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package extendsaula;
+
+/**
+ *
+ * @author hever
+ */
+public class Aplicacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ UCB -Eng/Orientada Objetos/extendsAula/src/extendsaula/Professor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package extendsaula;
+
+/**
+ *
+ * @author Hevert.Sousa
+ */
+public class Professor {
+
+}
